6c2531d43a83aee6c653b8e46295677910c98ef1
Yash Raj Singh <yashrajsingh807@gmail.com>
2021-06-06 17:35:13 +0530

    Code about broadcast receiver in android.

---
+++ app/src/main/java/com/example/broadcastreciever/AirplaneModeChangeReceiver.kt
@@ -0,0 +1,20 @@
+package com.example.broadcastreciever
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class AirplaneModeChangeReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val isAirenalbled = intent?.getBooleanExtra("state", false)?:return
+        if (isAirenalbled)
+        {
+            Toast.makeText(context, "Airplane Mode Enabled!", Toast.LENGTH_LONG).show()
+        }
+        else
+        {
+            Toast.makeText(context, "Airplane Mode Disabled!", Toast.LENGTH_LONG).show()
+        }
+    }
+}
